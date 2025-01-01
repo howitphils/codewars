4e61bc5464e6237163f7ac2a62a26ef4b78d747a
howitphils <uladzislauhr@gmail.com>
2025-01-01 19:28:34 +0300

    js challs

--- index.js
+++ index.js
@@ -161,4 +161,14 @@ function countBy(x, n) {
   return z;
 }
 
+var min = function(list){
+  list.sort((a,b) => a - b)
+  return list[0];
+}
+
+var max = function(list){
+  list.sort((a,b) => b - a)
+  return list[0];
+}
+
 console.log(simpleMultiplication(2));
