bc1f69d78f69f634b5d6514b6c15a3629aab6703
howitphils <uladzislauhr@gmail.com>
2025-01-04 23:13:28 +0300

    js

--- index.js
+++ index.js
@@ -257,4 +257,17 @@ function oddOrEven(array) {
   //enter code here
 }
 
+const quarterOf = (month) => {
+  // Your code here
+  if (month >= 1 && month <= 3) {
+    return 1;
+  } else if (month > 3 && month <= 6) {
+    return 2;
+  } else if (month > 6 && month <= 9) {
+    return 3;
+  } else {
+    return 4;
+  }
+};
+
 console.log(expandedForm(12345));
