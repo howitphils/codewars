ea9b68b2cef4f3bcb5a5c78ac1c5747777bb037e
howitphils <uladzislauhr@gmail.com>
2025-01-09 14:13:45 +0300

    js

--- index.js
+++ index.js
@@ -327,4 +327,11 @@ function solution(string) {
     .join("");
 }
 
-console.log(solution("hiHheEo"));
+function hoopCount(n) {
+  //your code goes here
+  return n >= 10
+    ? "Great, now move on to tricks"
+    : "Keep at it until you get it";
+}
+
+console.log(hoopCount(2));
