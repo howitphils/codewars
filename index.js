c29df0af1b437e156540690111a221a166abb5f1
howitphils <uladzislauhr@gmail.com>
2025-01-10 14:33:20 +0300

    js

--- index.js
+++ index.js
@@ -371,4 +371,9 @@ function isIsogram(str) {
   );
 }
 
+function getAge(inputString) {
+  // return the girl's correct age as an integer. Happy coding :)
+  return Number(inputString.split(" ")[0]);
+}
+
 console.log(isIsogram("dzxcvbnm"));
