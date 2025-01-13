8819ab6f836aed7506c9bc5d14c4bf06891288e2
howitphils <uladzislauhr@gmail.com>
2025-01-13 22:32:37 +0300

    js

--- index.js
+++ index.js
@@ -513,6 +513,16 @@ function openOrSenior(data) {
   // ...
 }
 
+function saleHotdogs(n) {
+  if (n < 5) {
+    return n * 100;
+  } else if (n >= 5 && n < 10) {
+    return n * 95;
+  } else if (n >= 10) {
+    return n * 90;
+  }
+}
+
 console.log(
   openOrSenior([
     [18, 20],
