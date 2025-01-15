e4d8593d29ab2c6fff3ab98884181d9be84d81f9
howitphils <uladzislauhr@gmail.com>
2025-01-15 19:05:32 +0300

    js

--- index.js
+++ index.js
@@ -558,4 +558,20 @@ function sortArray(array) {
   // Return a sorted array.
 }
 
-console.log(sortArray([2, 5, 3, 1, 4, 5, 2])); // []
+function gooseFilter(birds) {
+  var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+  return birds.filter((bird) => !geese.includes(bird));
+  // return an array containing all of the strings in the input array except those that match strings in geese
+}
+
+console.log(
+  gooseFilter([
+    "Mallard",
+    "Hook Bill",
+    "African",
+    "Crested",
+    "Pilgrim",
+    "Toulouse",
+    "Blue Swedish",
+  ])
+); //
