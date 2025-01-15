46eb994c998fa86f2a7eddc887026e06dc959e1d
howitphils <uladzislauhr@gmail.com>
2025-01-15 19:25:32 +0300

    js

--- index.js
+++ index.js
@@ -587,4 +587,12 @@ function expressionMatter(a, b, c) {
   return; // highest achievable result
 }
 
-console.log(expressionMatter(1, 2, 3)); //
+// write the function isAnagram
+var isAnagram = function (test, original) {
+  return (
+    test.toLowerCase().split("").sort().join("") ===
+    original.toLowerCase().split("").sort().join("")
+  );
+};
+
+console.log(isAnagram("asdsad", "qwead")); //
