3f607a558777f501dffd4c4f6631f4a0309757e0
howitphils <uladzislauhr@gmail.com>
2025-01-17 09:43:43 +0300

    js

--- index.js
+++ index.js
@@ -641,4 +641,16 @@ function roundToNext5(n) {
   return b;
 }
 
-console.log(roundToNext5(-2)); //
+function peopleWithAgeDrink(old) {
+  if (old < 14) {
+    return "drink toddy";
+  } else if (old < 18) {
+    return "drink coke";
+  } else if (old < 21) {
+    return "drink beer";
+  } else {
+    return "drink whisky";
+  }
+}
+
+console.log(peopleWithAgeDrink(10)); //
