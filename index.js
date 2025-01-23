ffd90dc94e0f9b3c83335212f17b48e362ca04a3
howitphils <uladzislauhr@gmail.com>
2025-01-23 19:20:53 +0300

    js

--- index.js
+++ index.js
@@ -883,4 +883,8 @@ function remove(string) {
   return string[string.length - 1] === "!" ? string.slice(0, -1) : string;
 }
 
-console.log(remove("Hello!")); //
+function arrayDiff(a, b) {
+  return a.filter((n) => !b.includes(n));
+}
+
+console.log(arrayDiff([1, 2], [1])); //
