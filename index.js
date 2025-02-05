7d055adcb95aa3def394b77bb878ea208f1ae2e8
howitphils <uladzislauhr@gmail.com>
2025-02-05 20:36:59 +0300

    js

--- index.js
+++ index.js
@@ -1453,4 +1453,13 @@ function mango(quantity, price) {
   return sum;
 }
 
-console.log(mango(2, 9)); //
+var countSheep = function (num) {
+  //your code here
+  let str = "";
+  for (let i = 1; i <= num; i++) {
+    str += `${i} sheep...`;
+  }
+  return str;
+};
+
+console.log(countSheep(50)); //
