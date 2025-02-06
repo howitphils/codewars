4ca54a539532305f0bccc00d884c15b4f738b1a4
howitphils <uladzislauhr@gmail.com>
2025-02-06 22:47:50 +0300

    js

--- index.js
+++ index.js
@@ -1531,5 +1531,10 @@ function stringClean(s) {
     .filter((l) => l !== "0" && !parseInt(l))
     .join("");
 }
+function sortByLength(array) {
+  // Return an array containing the same strings,
+  // ordered from shortest to longest
+  return array.sort((a, b) => a.length - b.length);
+}
 
-console.log(stringClean("dasa21wdzsd23r")); //
+console.log(sortByLength(["adzcx", "sad", "xzcqw", "qqq"])); //
