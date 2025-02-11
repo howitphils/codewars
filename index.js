e17715f99555edd843b7a421622713ab436c47cf
howitphils <uladzislauhr@gmail.com>
2025-02-11 22:28:27 +0300

    js

--- index.js
+++ index.js
@@ -1746,4 +1746,14 @@ function digits(n) {
   return String(n).length;
 }
 
-console.log(digits(999));
+function sumCubes(n) {
+  let sum = 0;
+
+  for (let i = 1; i <= n; i++) {
+    sum += Math.pow(i, 3);
+  }
+  return sum;
+  // ...
+}
+
+console.log(sumCubes(22));
