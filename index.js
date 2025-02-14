5b728afd861816b7f6c82cecf5898c97c2c91c06
howitphils <uladzislauhr@gmail.com>
2025-02-14 18:18:05 +0300

    js

--- index.js
+++ index.js
@@ -1829,4 +1829,9 @@ function countRedBeads(n) {
   return count;
 }
 
-console.log(countRedBeads(90));
+function remainder(n, m) {
+  // Divide the larger argument by the smaller argument and return the remainder
+  return n > m ? n % m : m % n;
+}
+
+console.log(remainder(20, 21));
