621eac62f5123730be9c817a39e576580561e5bc
howitphils <uladzislauhr@gmail.com>
2025-02-15 17:12:40 +0300

    js

--- index.js
+++ index.js
@@ -1900,4 +1900,10 @@ function remove(s, n) {
   return s;
 }
 
-console.log(remove("asd!!a!sd!", 3));
+function billboard(name, price = 30) {
+  let cost = 0;
+  for (let i = 0; i < name.length; i++) cost += price;
+  return cost;
+}
+
+console.log(billboard("asdasdk", 22));
