75b24f51fd8bb504d17918c903d84817f9f19be3
howitphils <uladzislauhr@gmail.com>
2025-02-19 22:05:22 +0300

    js

--- index.js
+++ index.js
@@ -2074,4 +2074,8 @@ function isPangram(string) {
   //...
 }
 
-console.log(isPangram("The quick brown fox jumps over the lazy dog."));
+function first(arr, n = 1) {
+  return arr.slice(0, n);
+}
+
+console.log(first([1, 2, 3, 4], 3));
