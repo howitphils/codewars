9218bbb89b1b2c1da67d77f66213e0698e7e503f
howitphils <uladzislauhr@gmail.com>
2025-02-24 22:20:31 +0300

    js

--- index.js
+++ index.js
@@ -2238,4 +2238,40 @@ Sleigh.prototype.authenticate = function (name, password) {
   return name === "Santa Claus" && password === "Ho Ho Ho!";
 };
 
-console.log(uniTotal("czxc"));
+function switchItUp(number) {
+  //Write your own Code!
+  switch (number) {
+    case 1: {
+      return "One";
+    }
+    case 2: {
+      return "Two";
+    }
+    case 3: {
+      return "Three";
+    }
+    case 4: {
+      return "Four";
+    }
+    case 5: {
+      return "Five";
+    }
+    case 6: {
+      return "Six";
+    }
+    case 7: {
+      return "Seven";
+    }
+    case 8: {
+      return "Eight";
+    }
+    case 9: {
+      return "Nine";
+    }
+    case 0: {
+      return "Zero";
+    }
+  }
+}
+
+console.log(switchItUp(9));
