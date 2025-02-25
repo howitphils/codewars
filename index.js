c82c827c9278ddf24cbf5442778241c6218fcbca
howitphils <uladzislauhr@gmail.com>
2025-02-25 22:25:03 +0300

    js

--- index.js
+++ index.js
@@ -2298,4 +2298,15 @@ function leo(oscar) {
   }
 }
 
+function aliasGen(first, last) {
+  if (
+    !/[A-Z]/.test(first[0].toUpperCase()) ||
+    !/[A-Z]/.test(last[0].toUpperCase())
+  )
+    return "Your name must start with a letter from A - Z.";
+  return (
+    firstName[first[0].toUpperCase()] + " " + surname[last[0].toUpperCase()]
+  );
+}
+
 console.log(leo(88));
