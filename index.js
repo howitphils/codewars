b43ce1c6f71e9e52c0eeeb1aa73bd854b4eac37d
howitphils <uladzislauhr@gmail.com>
2025-02-25 22:19:17 +0300

    js

--- index.js
+++ index.js
@@ -2286,4 +2286,16 @@ function evenNumbers(array, number) {
   return array.filter((item) => item % 2 === 0).slice(-number);
 }
 
-console.log(evenNumbers([1, 2, 3, 4, 5, 6, 7, 8], 2));
+function leo(oscar) {
+  if (oscar === 88) {
+    return "Leo finally won the oscar! Leo is happy";
+  } else if (oscar === 86) {
+    return "Not even for Wolf of wallstreet?!";
+  } else if (oscar < 88) {
+    return "When will you give Leo an Oscar?";
+  } else if (oscar > 88) {
+    return "Leo got one already!";
+  }
+}
+
+console.log(leo(88));
